5986e09ebb0c42bae2bd8a8aec66c595a494a95f
SrLuisito <agueromartinezluis9@gmail.com>
2023-06-23 13:58:27 -0300

    mercadoLiebre Terminado

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-const express = require ('express');
+const { request, response } = require("express");
+const path = require(`path`);
+const express = require(`express`);
 const app = express();
-const port = 3000
-const path = require ('path');
+const port = process.env.PORT || 3030;
 
+app.listen(port,() => console.log("http://localhost:3030/"));
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.listen(port,() => {
-    console.log("servidor funcionando en puerto " + port);
-})
+app.use(express.static(`Public`));
+/*app.use(express.static(path.resolve(__dirname,'../public')))*/
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + '/views/home.html')
-    res.sendFile(__dirname + '/views/login.html')
-    res.sendFile(__dirname + '/views/home.html')
-    res.sendFile(__dirname + '/views/home.html')
-})
+app.get(`/`, (request, response) => response.sendFile(path.resolve(__dirname, "./views/home.html")));
+app.get(`/ingresa`, (request, response) => response.sendFile(path.resolve(__dirname, "./views/login.html")));
+app.get(`/crea_tu_cuenta`, (request, response) => response.sendFile(path.resolve(__dirname, "./views/registro.html")));
+
+app.get(`/ofertas`);
+app.get(`/tiendas oficiales`);
+app.get(`/vender`);
+app.get(`/ayuda`);
+
+app.get(`/mis compras`);
